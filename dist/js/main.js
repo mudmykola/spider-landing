7893bcd4769013426709182d72fa4b4e66d78b90
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-05-11 18:09:43 +0300

    done=>team-partners

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
-
-
-
-  
+let swiper = new Swiper(".team-inners__slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    // centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 });
