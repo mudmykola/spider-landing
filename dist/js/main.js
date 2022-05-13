07b7e41948b0af8009daec59f70d64a9c6d8446b
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-05-14 01:31:53 +0300

    done=>ask

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,3 +11,19 @@ let swiper = new Swiper(".team-inners__slider", {
         disableOnInteraction: false,
     },
 });
+
+
+var acc = document.getElementsByClassName("ask-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
